c2f1d5e5a261dbaf943298107b8ca3b71b5f79e8
RobertJephthaHogan <robertjephthahogan@gmail.com>
2023-09-19 18:23:37 -0400

    landing page

--- src/client/src/pages/Homepage/index.tsx
+++ src/client/src/pages/Homepage/index.tsx
@@ -1,12 +1,34 @@
+import { Button } from "antd";
 import HomepageHeader from "../../components/HomepageHeader";
-
+import './styles.css'
 
 
 export function Homepage() {
     return (
         <div>
             <HomepageHeader/>
-            Homepage Body here
+            <div className="hp-landing">
+                <div className="hp-landing-content">
+                    <span className="landing-title">
+                        Ready to get started?
+                    </span>
+                    <span className="landing-subtitle">
+                        Time to get fit
+                    </span>
+                    <div className="landing-btn-row">
+                        <Button
+                            className="hp-btn"
+                        >
+                            Get Started
+                        </Button>
+                        <Button
+                            className="hp-btn"
+                        >
+                            Login
+                        </Button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
