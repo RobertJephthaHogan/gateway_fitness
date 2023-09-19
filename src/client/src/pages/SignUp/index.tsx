ca02b1152fd67da0cbb53aa360637afc9863169d
RobertJephthaHogan <robertjephthahogan@gmail.com>
2023-09-19 19:45:52 -0400

    signup page ui

--- src/client/src/pages/SignUp/index.tsx
+++ src/client/src/pages/SignUp/index.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { userService } from "../../services";
 import { User } from "../../types";
 import './styles.css'
+import { Button, Input } from "antd";
 
 
 
@@ -44,69 +45,47 @@ export function SignUp() {
 
 
     return (
-        <div className='sign-up-wrapper'>
-            <div className='left-side'>
-
-            </div>
-            <div className='right-side'>
-                <div className='form-container'>
-                    <form onSubmit={onSubmitSignup}>
-                        <div className='form-row'>
-                            <label> First Name: </label>
-                            <input
-                                name="firstName"
-                                id="firstName"
-                                type="text"
-                                value={userInfo?.firstName}
-                                onChange={(e) => onUserInfoChange(e?.target?.value, 'firstName')}
-                            />
-                        </div>
-                        <div className='form-row'>
-                            <label> Last Name: </label>
-                            <input
-                                name="lastName"
-                                id="lastName"
-                                type="text"
-                                value={userInfo?.lastName}
-                                onChange={(e) => onUserInfoChange(e?.target?.value, 'lastName')}
-                            />
-                        </div>
-                        <div className='form-row'>
-                            <label> Email: </label>
-                            <input
-                                name="email"
-                                id="email"
-                                type="text"
-                                value={userInfo?.email}
-                                onChange={(e) => onUserInfoChange(e?.target?.value, 'email')}
-                            />
-                        </div>
-                        <div className='form-row'>
-                            <label> Password: </label>
-                            <input
-                                name="password"
-                                id="password"
-                                type="text"
-                                value={userInfo?.password}
-                                onChange={(e) => onUserInfoChange(e?.target?.value, 'password')}
-                            />
-                        </div>
-                        <div className='form-row'>
-                            <label> Phone Number: </label>
-                            <input
-                                name="phoneNumber"
-                                id="phoneNumber"
-                                type="text"
-                                value={userInfo?.phoneNumber}
-                                onChange={(e) => onUserInfoChange(e?.target?.value, 'phoneNumber')}
-                            />
-                        </div>
-                        <div className='form-row'>
-                            <button type="submit" className='submit-btn hcp'>
-                                Submit
-                            </button>
-                        </div>
-                    </form>
+        <div className='signup-component'>
+            <div className="signup-content">
+                <div className="signup-content-title-wrapper">
+                    <span className="signup-content-title">Sign Up</span>
+                </div>
+                <div>
+                    <Input
+                        placeholder="First Name"
+                        className="signup-field"
+                    />
+                </div>
+                <div>
+                    <Input
+                        placeholder="Last Name"
+                        className="signup-field"
+                    />
+                </div>
+                <div>
+                    <Input
+                        placeholder="Email Address"
+                        className="signup-field"
+                    />
+                </div>
+                <div>
+                    <Input
+                        placeholder="Password"
+                        className="signup-field"
+                    />
+                </div>
+                <div>
+                    <Input
+                        placeholder="Confirm Password"
+                        className="signup-field"
+                    />
+                </div>
+                <div className="signup-btn-container">
+                    <Button
+                        className="signup-btn"
+                    >
+                        Sign up
+                    </Button>
                 </div>
             </div>
         </div>
