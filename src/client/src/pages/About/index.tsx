8ea4db372c4aff2019d1c907fa1641f15fccbeaf
RobertJephthaHogan <robertjephthahogan@gmail.com>
2023-09-26 19:19:42 -0400

    about us banner

--- src/client/src/pages/About/index.tsx
+++ src/client/src/pages/About/index.tsx
@@ -1,15 +1,37 @@
 import React from 'react'
 import HomepageHeader from '../../components/HomepageHeader'
-
+import './styles.css'
 
 
 
 export default function About() {
 
     return (
-        <div>
+        <div className='about-component'>
             <HomepageHeader/>
-            About Page
+            <div className='about-component-content-wrapper'>
+                <div className='about-component-content'>
+
+                    <div className='top-banner-wrapper'>
+                        <div className='top-banner'>
+                            <div className='about-title-wrapper'>
+                                <span className='about-title'>
+                                    About Us
+                                </span>
+                            </div>
+                            <div className='about-description-wrapper'>
+                                <span className='about-us-description'>
+                                    Here at Gateway Fitness we believe that fitness is the gateway to an awesome life.  
+                                    We provide online tools to help you easily track your workouts and nutrition, 
+                                    tailored workout and nutrition plans based on your goals, 
+                                    and supplements to help you speed up your progress.
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
     )
 }
