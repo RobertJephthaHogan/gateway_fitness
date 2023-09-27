4827ae4bc1d75bc9c1908810d2c98167e2de796e
RobertJephthaHogan <robertjephthahogan@gmail.com>
2023-09-27 13:43:53 -0400

    About Page latest

--- src/client/src/pages/About/index.tsx
+++ src/client/src/pages/About/index.tsx
@@ -36,9 +36,7 @@ export default function About() {
                                 Our Approach
                             </span>
                         </div>
-                        <div className='appr-space'>
-
-                        </div>
+                        <div className='appr-space'></div>
                         <div className='approach-row'>
                             <div className='approach-row-text-left'>
                                 <div className='approach-row-top-text-container'>
@@ -53,7 +51,8 @@ export default function About() {
                                 </div>
                                 <div  className='approach-row-bottom-text-container'>
                                     <span className='approach-row-bottom-text'>
-                                        We’ve created a high-tech fitness platform with ergonomic tools for planning and tracking your workouts and your nutrition. We provide a free “self-managed” tier, in addition to our premium tiers that include our tailored features and templates.
+                                        We’ve created a high-tech fitness platform with ergonomic tools for planning and tracking your workouts and your nutrition. 
+                                        We provide a free “self-managed” tier, in addition to our premium tiers that include our tailored features and templates.
                                     </span>
                                 </div>
                             </div>
@@ -61,6 +60,62 @@ export default function About() {
                                 
                             </div>
                         </div>
+                        <div className='approach-row'>
+                            <div className='approach-row-image-left'>
+                                
+                            </div>
+                            <div className='approach-row-text-right'>
+                                <div className='approach-row-text-right-container'>
+                                    <div className='approach-row-top-text-container'>
+                                        <span className='approach-row-top-text'>
+                                            2. Make it Customizable
+                                        </span>
+                                    </div>
+                                    <div className='approach-row-mid-text-container'>
+                                        <span className='approach-row-mid-text'>
+                                            Tailor our platform for your fitness goals
+                                        </span>
+                                    </div>
+                                    <div  className='approach-row-bottom-text-container'>
+                                        <span className='approach-row-bottom-text'>
+                                            We understand that not everyone has the same fitness goals, and that your fitness goals may change over time. 
+                                            Our premium tiers allow you to schedule entire nutrition and workout plans based on your goals at the click of a button. 
+                                            If your goals change, you can re-generate your workout and nutrition plans quickly and easily.
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='approach-row'>
+                            <div className='approach-row-text-left'>
+                                <div className='approach-row-text-left-container'>
+                                    <div className='approach-row-top-text-container'>
+                                        <span className='approach-row-top-text'>
+                                            3.Provide online trainers
+                                        </span>
+                                    </div>
+                                    <div className='approach-row-mid-text-container'>
+                                        <span className='approach-row-mid-text'>
+                                            We provide online personal trainers to help keep you on track
+                                        </span>
+                                    </div>
+                                    <div  className='approach-row-bottom-text-container'>
+                                        <span className='approach-row-bottom-text'>
+                                            Getting started, or getting back on track with a fitness routine can be a challenge. 
+                                            Our trainers can help you get on track easily and quickly, help you find an effective routine, 
+                                            and ensure you understand proper form for all of your exercises.
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='approach-row-image-right'>
+                                
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='spce-btm'>
+
                     </div>
 
                 </div>
