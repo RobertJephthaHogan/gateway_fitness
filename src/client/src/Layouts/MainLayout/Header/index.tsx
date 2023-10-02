2028b7c27b5debed149904a8ba384d76bf347466
RobertJephthaHogan <robertjephthahogan@gmail.com>
2023-10-02 18:38:16 -0400

    layout header latest

--- src/client/src/Layouts/MainLayout/Header/index.tsx
+++ src/client/src/Layouts/MainLayout/Header/index.tsx
@@ -6,17 +6,28 @@ export default function LayoutHeader() {
 
     return (
         <div className="layout-header">
-            <div className="layout-header-left">
-                <div>
-                    <span className="lo-header-title-bold">Gateway</span>
-                    <span  className="lo-header-title-light">Fitness</span>
-                </div>
-                <div>
-                    <span className="lo-header-subtitle">
-                        The Gateway To A Healthier Life
+            <div className='layout-header-left layout-header-cell'>
+                <div className='layout-header-left-wrapper'>
+                    <span className='header-left-title-text'>
+                        <span className='header-left-title-bold'>
+                            Gateway
+                        </span>
+                        <span className='header-left-title-light'>
+                            Fitness
+                        </span>
+                    </span>
+                    <span className='header-left-subtitle-text'>
+                        The Gateway to a Healthier life
                     </span>
                 </div>
             </div>
+            <div className='layout-header-center layout-header-cell'>
+
+            </div>
+            <div className='layout-header-right layout-header-cell'>
+                Hp header right
+            </div>
+
         </div>
     )
 }
