298bd8a9f2ecebf3983f135e8223ea24297cfe70
RobertJephthaHogan <robertjephthahogan@gmail.com>
2023-10-02 19:13:28 -0400

    sidebar latest

--- src/client/src/Layouts/MainLayout/SideBar/index.tsx
+++ src/client/src/Layouts/MainLayout/SideBar/index.tsx
@@ -1,12 +1,50 @@
 import React from 'react'
 import './styles.css'
+import { useNavigate } from 'react-router'
 
 
 export default function SideBar() {
 
+    const navigate = useNavigate()
+
+    const menuItems = [
+        {
+            title: 'Dashboard',
+            route: '/dashboard'
+        },
+        {
+            title: 'My Workouts',
+            route: '/my-workouts'
+        },
+        {
+            title: 'My Nutrition',
+            route: '/my-nutrition'
+        },
+        {
+            title: 'Workout Plans',
+            route: '/workout-plans'
+        },
+        {
+            title: 'Nutrition Plans',
+            route: '/nutrition-plans'
+        },
+    ]
+
+
     return (
         <div className='sidebar'>
-            SideBar
+            {
+                menuItems?.map((item: any) => {
+                    return (
+                        <div 
+                            className='sidebar-row'
+                            onClick={() => navigate(item?.route)}
+                        >
+                            <span className='sidebar-row-title'>{item?.title}</span>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
