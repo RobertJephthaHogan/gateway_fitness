326bc9e1a66e3afcc4f3b0c342b112e24fee20ae
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-10-20 16:14:59 -0400

    Login form integration

--- src/client/src/pages/Login/index.tsx
+++ src/client/src/pages/Login/index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { userService } from "../../services"
 import './styles.css'
-import { Button, Input } from "antd"
+import { Button, Form, Input } from "antd"
 
 
 export function Login() {
@@ -10,26 +10,18 @@ export function Login() {
     const [loginInfo, setLoginInfo] = useState<any>({})
     const navigate = useNavigate()
 
-
-    function handleLoginInfoChange(data: any, field: any) {
-        let workingObj = {...loginInfo}
-        workingObj[field] = data
-        setLoginInfo(workingObj)     
-    }
-
     
     function dispatchLoginData(resp: any) {
         console.log('resp', resp)
 		//store.dispatch(userActions.login(resp))
     }
 
-
-    function onSubmitLogin(e : any) {
-        e.preventDefault()
-        userService.loginUser(loginInfo).then((resp: any) => {
-			dispatchLoginData(resp.data)
-			navigate('/dashboard')
-        })
+    function onFinish(e : any) {
+        console.log('e', e)
+        // userService.loginUser(loginInfo).then((resp: any) => {
+		// 	dispatchLoginData(resp.data)
+		// 	navigate('/dashboard')
+        // })
     }
 
 
@@ -39,30 +31,39 @@ export function Login() {
                 <div className="login-content-title-wrapper">
                     <span className="login-content-title">Log in</span>
                 </div>
-                <div>
-                    <Input
-                        placeholder="Email Address"
-                        className="login-email-field"
-                    />
-                </div>
-                <div>
-                    <Input
-                        placeholder="Password"
-                        className="login-password-field"
-                    />
-                </div>
-                <div>
-                    <span className="forgot-password-text">
-                        Forgot Password?
-                    </span>
-                </div>
-                <div className="login-btn-container">
-                    <Button
-                        className="login-btn"
-                    >
-                        Login
-                    </Button>
-                </div>
+                <Form 
+                    onFinish={onFinish}
+                >
+                    <div>
+                        <Form.Item name="email" rules={[{ required: true }]} className="login-form-item">
+                            <Input
+                                placeholder="Email Address"
+                                className="login-email-field"
+                            />
+                        </Form.Item>
+                    </div>
+                    <div>
+                        <Form.Item name="password" rules={[{ required: true }]} className="login-form-item">
+                            <Input
+                                placeholder="Password"
+                                className="login-password-field"
+                            />
+                        </Form.Item>
+                    </div>
+                    <div>
+                        <span className="forgot-password-text">
+                            Forgot Password?
+                        </span>
+                    </div>
+                    <div className="login-btn-container">
+                        <Button
+                            className="login-btn"
+                            htmlType="submit"
+                        >
+                            Login
+                        </Button>
+                    </div>
+                </Form>
             </div>
         </div>
     )
