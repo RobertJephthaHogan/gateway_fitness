568544f90182ffdb673fd911661dcee1157e0be3
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-10-27 19:16:26 -0400

    calendar topbar UI components

--- src/client/src/components/Calendar/index.tsx
+++ src/client/src/components/Calendar/index.tsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import './styles.css'
+import { Button, DatePicker } from 'antd'
+import LeftOutlined from '@ant-design/icons/LeftOutlined'
+import RightOutlined from '@ant-design/icons/RightOutlined'
+
+
 
 export default function Calendar() {
 
     return (
         <div className='calendar'>
+            <div className='calendar-top-bar'>
+                <div className='calendar-top-bar-left'>
+                    <span className='calendar-date-text'>
+                        2023 - Week 39
+                    </span>
+                </div>
+                <div className='calendar-top-bar-right'>
+                    <Button size='small'>
+                        <LeftOutlined/>
+                    </Button>
+                    <DatePicker 
+                        size='small'
+                        //onChange={onChange} 
+                        picker="week" 
+                        className='calendar-datepicker'
+                    />
+                    <Button size='small'>
+                        <RightOutlined/>
+                    </Button>
+                </div>
+            </div>
+            <div className='calendar-date-bar'>
+
+            </div>
             Calendar
         </div>
     )
