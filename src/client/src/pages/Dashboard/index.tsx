5db1c15a805e3783420a91370e75568763e1b38a
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-10-27 17:36:37 -0400

    dashboard layout

--- src/client/src/pages/Dashboard/index.tsx
+++ src/client/src/pages/Dashboard/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
-
+import './styles.css'
 
 export default function Dashboard() {
 
     return (
-        <div>
-            Dashboard
+        <div className='dashboard'>
+            <div className='calendar-and-card-container'>
+                <div className='calendar-container'>
+                    Calendar Container
+                </div>
+                <div className='card-container'>
+                    Card Container
+                </div>
+            </div>
         </div>
     )
 }
