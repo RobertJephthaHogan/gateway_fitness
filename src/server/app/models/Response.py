5e8cdf8fa7724e06a1084c3b0e41d2cd506f146c
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-10-29 16:03:14 -0400

    Meal route integration, Response models

--- src/server/app/models/Response.py
+++ src/server/app/models/Response.py
@@ -19,3 +19,35 @@ class Response(BaseModel):
         }
 
 
+class Response200(BaseModel):
+    status_code: int = 200
+    response_type: str = "Success"
+    description: str = "Operation Success"
+    data: Optional[Any]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "status_code": 200,
+                "response_type": "success",
+                "description": "Operation successful",
+                "data": "data"
+            }
+        }
+
+
+class Response404(BaseModel):
+    status_code: int = 404
+    response_type: str = "Error"
+    description: str = "Operation Failed"
+    data: Optional[Any]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "status_code": 404,
+                "response_type": "Error",
+                "description": "Operation Failed",
+                "data": "data"
+            }
+        }
