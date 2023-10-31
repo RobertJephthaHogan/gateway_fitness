c5f73f36aed06f14f94864192b8a7bb0089eeb8b
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-10-31 18:10:54 -0400

    Nutrition Card topbar

--- src/client/src/components/NutritionCard/index.tsx
+++ src/client/src/components/NutritionCard/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
-
+import './styles.css'
+import { PlusSquareOutlined } from '@ant-design/icons'
 
 export default function NutritionCard() {
 
     return (
-        <div>
+        <div className='nutrition-card'>
+            <div className='nutrition-card-topbar'>
+                <div className='nc-topbar-left'>
+                    <span className='nc-tb-text nc-tb-title'>Today's Nutrients</span>
+                    <span className='nc-tb-text nc-tb-sub'>Calories Planned : 2750</span>
+                    <span className='nc-tb-text nc-tb-sub'>Meals Planned: 6 </span>
+                </div>
+                <div className='nc-topbar-right'>
+                    <PlusSquareOutlined className='nc-tb-plus'/>
+                </div>
+            </div>
             Nutrition Card
+
         </div>
     )
 }
