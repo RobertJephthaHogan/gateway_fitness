7c2ff555379f45a1d4d06d30215194871425c5eb
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-10-31 21:39:32 -0400

    Nutrition Form Inputs, input handling

--- src/client/src/components/forms/NutritionForm/index.tsx
+++ src/client/src/components/forms/NutritionForm/index.tsx
@@ -1,15 +1,48 @@
 import React, { useState } from 'react'
 import './styles.css'
 import { useSelector } from 'react-redux'
+import { DatePicker, Input } from 'antd'
 
 export default function NutritionForm() {
 
     const currentUser = useSelector((state: any) => state.user?.data ?? [])
     const [formValues, setFormValues] = useState<any>()
 
+    const handleInputChange = (field: string, value: any) => {
+
+        const workingObj = {...formValues}
+        workingObj[field] = value
+        console.log('workingObj', workingObj)
+        setFormValues(workingObj)
+    }
+
+    const onFinish = () => {
+
+    }
+
     return (
         <div>
-            Nutrition Form
+            <Input
+                placeholder='Title'
+                onChange={
+                    (e) => handleInputChange(
+                        'title', 
+                        e?.target?.value
+                        )
+                }
+            />
+            <DatePicker 
+                showTime
+                onChange={
+                    (v) => handleInputChange(
+                        'time', v?.format('YYYY-MM-DD HH:mm:ss')
+                        )
+                } 
+                className='nutrition-form-datepicker'
+            />
+            <div>
+                ToDo: Ingredients component here
+            </div>
         </div>
     )
 }
